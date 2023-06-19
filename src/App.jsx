2975175c6f380f3a3c9ba2fba15c59bcd5d06060
alux444 <aluux999@gmail.com>
2023-06-19 17:18:42 +1200

    conditional rendering of main components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import "./App.css";
-import testDb from "./utils/testDb";
 import getAllItems from "./utils/getAllItems";
-import { useState } from "react";
 import convertDate from "./utils/convertDate";
+import { createContext, useState } from "react";
+import AdminDashboard from "./components/admin-dashboard/AdminDashboard";
+import StoreDisplay from "./components/store-display/StoreDisplay";
+
+const AdminContext = createContext();
 
 function App() {
-  const [data, setData] = useState();
+  const [admin, setAdmin] = useState(false);
 
   const getResults = async () => {
     const results = await getAllItems();
-    setData(results);
     console.log(results);
     console.log(convertDate(results[0].timeCreated.seconds));
   };
 
   return (
-    <>
-      <button onClick={testDb}>Test Database</button>
-      <button onClick={getResults}>Test getitems</button>
+    <AdminContext.Provider value={{ admin, setAdmin }}>
+      <button onClick={getResults}>Test get items</button>
+      <br />
+      <button onClick={() => setAdmin(!admin)}>Set Admin (for testing)</button>
       <p className="text-red-500">Red Font</p>
-    </>
+      {admin ? <AdminDashboard /> : <StoreDisplay />}
+    </AdminContext.Provider>
   );
 }
 
