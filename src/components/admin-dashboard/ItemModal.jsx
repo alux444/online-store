70fbe8dc70f8019cd2c1c2d2002c1bfe756c6723
Jamie <eejl773@aucklanduni.ac.nz>
2023-06-20 23:54:10 +1200

    added styling to modal component

--- src/components/admin-dashboard/ItemModal.jsx
+++ src/components/admin-dashboard/ItemModal.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { Modal, Button } from "@mui/material";
 
 const ItemModal = ({ item, onClose }) => {
   return (
     <Modal open={true} onClose={onClose}>
-      <div className="modal">
-        <div className="modal-content">
-          <h2>{item && item.name}</h2>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <div className="bg-white rounded-lg p-6 w-1/2 relative">
+          <Button onClick={onClose} className="absolute top-2 right-2">
+            X
+          </Button>
+          <h2 className="text-xl font-bold mb-2">{item && item.name}</h2>
           <p>{item && item.description}</p>
-          <Button onClick={onClose}>Close</Button>
+          <p>${item && item.price}</p>
         </div>
       </div>
     </Modal>
