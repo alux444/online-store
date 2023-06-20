ffde0066862c18ac14eff66a9586f01de7b159a5
Jamie <eejl773@aucklanduni.ac.nz>
2023-06-20 23:07:17 +1200

    styled dropdown menu for item search

--- src/components/admin-dashboard/ItemSearch.jsx
+++ src/components/admin-dashboard/ItemSearch.jsx
@@ -31,30 +31,38 @@ const ItemSearch = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleChange}>
-        <input
-          type="text"
-          placeholder="Search for an item..."
-          value={search}
-          onChange={(event) => setSearch(event.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-      {search && (
-        <div className="search-results">
-          <ul>
-            {searchResults.length > 0 ? (
-              searchResults.map((item) => (
-                <li key={item.id}>{item.name}</li>
-              ))
-            ) : (
-              <li>No results found.</li>
-            )}
-          </ul>
-        </div>
-      )}
-      
+    <div className="flex h-screen justify-center">
+      <div className="relative">
+        <form onSubmit={handleChange} className="flex items-center">
+          <input
+            type="text"
+            placeholder="Search for an item..."
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            className="px-4 py-2 rounded-l "
+
+          />
+          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-r">
+            Search
+          </button>
+          
+        </form>
+        {search && (
+            <div className="text-left mt-2 absolute z-10 w-full bg-white border border-gray-300 shadow-md">
+              <ul className="py-1">
+                {searchResults.length > 0 ? (
+                  searchResults.map((item) => (
+                    <li key={item.id} className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                      {item.name}
+                    </li>
+                  ))
+                ) : (
+                  <li className="px-4 py-2">No results found.</li>
+                )}
+              </ul>
+            </div>
+          )}
+      </div>
     </div>
   );
 };
