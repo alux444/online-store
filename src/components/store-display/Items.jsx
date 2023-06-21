5cbbfdbcc6eae7d0ac2906a25d1d7b3ad850dc3a
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-06-21 19:45:34 +1200

    added clearance sorting

--- src/components/store-display/Items.jsx
+++ src/components/store-display/Items.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import getAllItems from "../../utils/getAllItems";
 import ItemDisplayStore from "./ItemDisplayStore";
 
-const Items = () => {
+const Items = ({sortingOption, checked}) => {
   const [allItems, setAllItems] = useState([]);
 
   const getItems = async () => {
@@ -18,10 +18,14 @@ const Items = () => {
     return <ItemDisplayStore item={item} key={item.id} />;
   });
 
+  const clearanceItemMap = allItems.filter((item) => item.clearance).map((item) => {
+    return <ItemDisplayStore item={item} key={item.id} />;
+  })
+
   return (
     <div>
       <div className="flex flex-wrap justify-center align-center items-center gap-2">
-        {itemMap}
+        {checked ? itemMap : clearanceItemMap}
       </div>
     </div>
   );
--- src/components/store-display/StoreDisplay.jsx
+++ src/components/store-display/StoreDisplay.jsx
@@ -4,6 +4,7 @@ import Cart from "./cart/Cart";
 
 const StoreDisplay = () => {
   const [sortingOption, setSortingOption] = useState("");
+  const [checked, setChecked] = useState(false);
 
   const handleSorting = (option) => {
     setSortingOption(option);
@@ -11,26 +12,43 @@ const StoreDisplay = () => {
     console.log(`sorting by ${option}`);
   };
 
+  const handleChange = () => {
+    setChecked(!checked);
+  }
+
   return (
     <div>
       <p>Store display.</p>
       <Cart />
-
-      <div>
-        <p>Filter by:</p>
-        <select
-          value={sortingOption}
-          onChange={(e) => handleSorting(e.target.value)}
-        >
-          <option value="name">Name</option>
-          <option value="price-low">Price (Low)</option>
-          <option value="price-high">Price (High)</option>
-          <option value="date-old">Date Added (Oldest)</option>
-          <option value="date-new">Date added (Newest)</option>
-        </select>
+      <div className="flex justify-center align-center">
+        <div>
+          <p>Filter by:</p>
+        </div>
+        <div className="mr-5">
+          <select
+            value={sortingOption}
+            onChange={(e) => handleSorting(e.target.value)}
+          >
+            <option value="date-old">Date Added (Oldest)</option>
+            <option value="date-new">Date added (Newest)</option>
+            <option value="price-low">Price (Low)</option>
+            <option value="price-high">Price (High)</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={handleChange}
+            />
+            Show clearance
+          </label>
+        </div>
       </div>
 
-      <Items />
+
+      <Items sortingOption={sortingOption} checked={checked}/>
     </div>
   );
 };
