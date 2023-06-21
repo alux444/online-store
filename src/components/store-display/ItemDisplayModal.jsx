86e8421b63bc67f31c975aee42e4e966b45e50aa
alux444 <aluux999@gmail.com>
2023-06-21 12:49:16 +1200

    item cart display

--- src/components/store-display/ItemDisplayModal.jsx
+++ src/components/store-display/ItemDisplayModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Modal } from "@mui/material";
 import useOutsideClick from "../../utils/useOutsideClose";
 
@@ -6,17 +6,37 @@ const ItemDisplayModal = ({ open, close, item }) => {
   const modalRef = useRef(null);
   useOutsideClick(modalRef, close);
 
+  const [count, setCount] = useState(1);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  const salePrice = item.price - item.discount;
+
+  const overallCost = count * (item.onSale ? salePrice : item.price);
+
   return (
     <Modal open={open}>
       <div className="w-screen h-screen flex align-center justify-center text-center items-center">
         <div
-          className="border-2 border-blue border-solid h-[min-content] bg-white"
+          className="border-2 border-blue border-solid h-[min-content] bg-white p-5 rounded-lg "
           ref={modalRef}
         >
           <h2>{item.name}</h2>
           <img src={item.imageUrl} className="max-w-[50vw] max-h[50vh]" />
-          <h2>{item.onSale ? item.price - item.discount : item.price} each.</h2>
+          <h2>{item.onSale ? salePrice : item.price} each.</h2>
           <p>{item.description}</p>
+          <p>Add: {count} to Cart</p>
+          <button onClick={incrementCount}>+</button>
+          <button onClick={decrementCount}>-</button>
+          <small>This will cost : ${overallCost}</small>
         </div>
       </div>
     </Modal>
