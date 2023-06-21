7f4353bdf16572ca4c9e22633efb1c064394e2ee
Jamie <eejl773@aucklanduni.ac.nz>
2023-06-21 13:41:30 +1200

    added delete confirmation modal

--- src/components/admin-dashboard/ItemModal.jsx
+++ src/components/admin-dashboard/ItemModal.jsx
@@ -4,6 +4,7 @@ import deleteItem from "../../utils/deleteItem";
 
 const ItemModal = ({ item, onClose }) => {
   const [edit, setEdit] = useState("");
+  const [checkDelete, setCheckDelete] = useState(false);
 
   const handleChange = (event) => {
     event.preventDefault();
@@ -31,30 +32,48 @@ const ItemModal = ({ item, onClose }) => {
 
   return (
     <Modal open={true} onClose={onClose}>
-      <div className="fixed inset-0 flex items-center justify-center">
-        <div className="bg-white rounded-lg p-6 w-1/2 relative">
-          <Button onClick={onClose} className="absolute top-2 right-2">
-            X
-          </Button>
-          <h2 className="text-xl font-bold mb-2">{item && item.name}</h2>
-          <form onSubmit={handleChange}>
-            <input
-              type="text"
-              placeholder={item && item.description}
-              value={edit}
-              onChange={(event) => setEdit(event.target.value)}
-              className="px-4 py-2 rounded-l border-none w-full text-gray-600"
-            />
-          </form>
-          <p>${item && item.price}</p>
-          <Button
-            onClick={handleDelete}
-            className="bg-red-500 text-white px-4 py-2 rounded-r"
-          >
-            Delete {item && item.name}
-          </Button>
+      {!checkDelete ? (
+        <div className="fixed inset-0 flex items-center justify-center">
+          <div className="bg-white rounded-lg p-6 w-1/2 relative">
+            <Button onClick={onClose} className="absolute top-2 right-2">
+              X
+            </Button>
+            <h2 className="text-xl font-bold mb-2">{item && item.name}</h2>
+            <form onSubmit={handleChange}>
+              <input
+                type="text"
+                placeholder={item && item.description}
+                value={edit}
+                onChange={(event) => setEdit(event.target.value)}
+                className="px-4 py-2 rounded-l border-none w-full text-gray-600"
+              />
+            </form>
+            <p>${item && item.price}</p>
+            <Button
+              onClick={() => setCheckDelete(true)}
+              className="bg-red-500 text-white px-4 py-2 rounded-r"
+            >
+              Delete {item && item.name}
+            </Button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="fixed inset-0 flex items-center justify-center">
+          <div className="bg-white rounded-lg p-6 w-1/2 relative">
+            <Button onClick={() => setCheckDelete(false)} className="absolute top-2 right-2">
+              X
+            </Button>
+            <h2 className="text-xl font-bold mb-2">{item && item.name}</h2>
+            <p>Are you sure you want to delete {item && item.name}?</p>
+            <Button
+              onClick={handleDelete}
+              className="bg-red-500 text-white px-4 py-2 rounded-r"
+            >
+              Yes, delete {item && item.name}
+            </Button>
+          </div>
+        </div>
+      )}
     </Modal>
   );
 };
