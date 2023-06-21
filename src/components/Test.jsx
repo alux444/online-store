86305ec7b327c6c4ae8e8afd52369828b60a0526
alux444 <aluux999@gmail.com>
2023-06-21 12:07:54 +1200

    basic list styling

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,14 +1,8 @@
 import React, { useState } from "react";
-import updateItem from "../utils/updateItem";
 
 const Test = () => {
   const test = async () => {
-    console.log(
-      await updateItem("863d2273-784f-4a08-8bd6-1da968df0c26", {
-        clearance: true,
-        description: "edited description is crazy",
-      })
-    );
+    console.log("hello");
   };
   return (
     <div>
