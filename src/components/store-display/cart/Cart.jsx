7874879b85b73f2e1826c638dee51503a82b1d36
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-06-23 09:56:09 +1200

    cart styling

--- src/components/store-display/cart/Cart.jsx
+++ src/components/store-display/cart/Cart.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import CartModal from "./CartModal";
+import { CartContext } from "../../../App";
+import image from "../../../../public/images/shopping-cart.png"
 
 const Cart = () => {
   const [showCart, setShowCart] = useState(false);
+
+  const { cart, setCart } = useContext(CartContext);
 
   const closeCart = () => {
     setShowCart(false);
   };
 
+  const total = cart.reduce((total, item) => {
+    return total + item.amount * item.price;
+  }, 0);
+
   return (
-    <div>
-      <button onClick={() => setShowCart(true)}>Show Cart</button>
-      <CartModal open={showCart} close={closeCart} />
+    <div className="flex justify-center items-center mb-2">
+      <button className="flex items-center font-bold text-lg" onClick={() => setShowCart(true)}>
+        <img src={image} className="w-10 mr-3" />
+        ${total}
+      </button>
+      <CartModal open={showCart} close={closeCart} total={total} />
     </div>
   );
 };
