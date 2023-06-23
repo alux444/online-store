89b83e39e60eefa8c3f3a5af4302b2ae7a6f4e47
Jamie <eejl773@aucklanduni.ac.nz>
2023-06-23 12:29:36 +1200

    updated list display

--- src/components/admin-dashboard/ItemDisplayList.jsx
+++ src/components/admin-dashboard/ItemDisplayList.jsx
@@ -5,21 +5,28 @@ const ItemDisplayList = ({ item, onOpenModal }) => {
     onOpenModal();
     console.log("Search query:", item.name);
   };
+  const toTitleCase = (str) => {
+    return str.replace(/\w\S*/g, (txt) => {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  };
 
   return (
-    <div className="flex border-[1px] items-center px-4 py-2 hover:bg-gray-100 cursor-pointer"
-          onClick={() => handleItemClick(item)}
-      >
-      <div className="mr-4 text-left w-2/3">
+    <div className="flex border-[1px] items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+      <div className="mr-4 text-left w-1/2">
         <p>{item.name}</p>
       </div>
-      <div className="text-left">
+      <div className="mr-4 text-left w-1/4">
         <p>
-        {item.clearance ? `CLEARANCE: $${item.price - item.discount}` : item.onSale ? `SALE: $${item.price - item.discount}` : "No Sale."}{" "}
+          Department: {toTitleCase(item.category)} <br />
+          Stock on hand: {item.stock}
+        </p>
+      </div>
+      <div className="text-left w-1/5">
+        <p>
+          {item.clearance ? `CLEARANCE: $${item.price - item.discount}` : item.onSale ? `SALE: $${item.price - item.discount}` : "No Sale."}{" "}
           <br />
           Original price: ${item.price}
-          <br />
-          Stock on hand: {item.stock}
         </p>
       </div>
     </div>
