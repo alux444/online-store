9807507e95beddace7f44e71723e4b15e061659a
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2023-06-24 15:32:23 +1200

    organising checkout and cart modals

--- src/components/store-display/cart/CartModal.jsx
+++ src/components/store-display/cart/CartModal.jsx
@@ -3,31 +3,11 @@ import React, { useState, useRef, useContext } from "react";
 import useOutsideClick from "../../../utils/useOutsideClose";
 import { CartContext } from "../../../App";
 import CartItemDisplay from "./CartItemDisplay";
-import Checkout from "../checkout/Checkout";
+import CheckoutModal from "../checkout/CheckoutModal";
 
 const CartModal = ({ open, close, total }) => {
   const { cart, setCart } = useContext(CartContext);
   const [showCheckout, setShowCheckout] = useState(false);
-  const [cardNumber, setCardNumber] = useState("");
-  const [name, setName] = useState("");
-  const [expirationDate, setExpirationDate] = useState("");
-  const [securityCode, setSecurityCode] = useState("");
-
-  const handleCardNumber = (e) => {
-    setCardNumber(e.target.value);
-  };
-
-  const handleName = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleExpirationDate = (e) => {
-    setExpirationDate(e.target.value);
-  };
-
-  const handleSecurityCode = (e) => {
-    setSecurityCode(e.target.value);
-  };
 
   const modalRef = useRef(null);
 
@@ -45,54 +25,7 @@ const CartModal = ({ open, close, total }) => {
           ref={modalRef}
         >
           {showCheckout ? (
-            <div className="flex align-center items-center justify-center">
-              <div className="border-2 border-white bg-white p-5">
-                <h1 className="text-2xl font-bold">Payment</h1>
-                <p>All transactions are secure and encrypted</p>
-                <form className="flex flex-col items-center space-y-4 mt-3">
-                  <label>
-                    <input
-                      className="w-96 rounded-lg sm:w-80"
-                      type="text"
-                      value={cardNumber}
-                      onChange={handleCardNumber}
-                      placeholder="Card number"
-                    />
-                  </label>
-                  <label>
-                    <input
-                      className="w-96 rounded-lg sm:w-80"
-                      type="text"
-                      value={name}
-                      onChange={handleName}
-                      placeholder="Name on card"
-                    />
-                  </label>
-                  <div>
-                    <label>
-                      <input
-                        className="w-58 rounded-lg mr-2 sm:w-48"
-                        type="text"
-                        value={expirationDate}
-                        onChange={handleExpirationDate}
-                        placeholder="Expiration Date (MM / YY)"
-                      />
-                    </label>
-                    <label>
-                      <input
-                        className="w-36 rounded-lg ml-1 sm:w-28"
-                        type="text"
-                        value={securityCode}
-                        onChange={handleSecurityCode}
-                        placeholder="Security code"
-                      />
-                    </label>
-                  </div>
-                  <button>Submit</button>
-                </form>
-                <button onClick={() => setShowCheckout(false)}>Return to cart</button>
-              </div>
-            </div>
+            <CheckoutModal setShowCheckout={setShowCheckout} />
           ) : (
             <div>
               <button onClick={() => setCart([])}>Clear Cart?</button>
--- src/components/store-display/checkout/CheckoutModal.jsx
+++ src/components/store-display/checkout/CheckoutModal.jsx
@@ -1,8 +1,7 @@
-import { Modal } from "@mui/material";
 import React, { useRef, useState } from "react";
 import useOutsideClick from "../../../utils/useOutsideClose";
 
-const CheckoutModal = ({ open, close }) => {
+const CheckoutModal = ({ setShowCheckout }) => {
   const [cardNumber, setCardNumber] = useState("");
   const [name, setName] = useState("");
   const [expirationDate, setExpirationDate] = useState("");
@@ -29,55 +28,54 @@ const CheckoutModal = ({ open, close }) => {
   useOutsideClick(modalRef, close);
 
   return (
-    <Modal open={open} className="flex align-center items-center justify-center">
-      <div className="h-screen w-screen flex align-center items-center justify-center">
-        <div className="border-2 border-white bg-white p-5" ref={modalRef}>
-          <h1 className="text-2xl font-bold">Payment</h1>
-          <p>All transactions are secure and encrypted</p>
-          <form className="flex flex-col items-center space-y-4 mt-3">
+    <div className="flex align-center items-center justify-center">
+      <div className="border-2 border-white bg-white p-5">
+        <h1 className="text-2xl font-bold">Payment</h1>
+        <p>All transactions are secure and encrypted</p>
+        <form className="flex flex-col items-center space-y-4 mt-3">
+          <label>
+            <input
+              className="w-96 rounded-lg sm:w-80"
+              type="text"
+              value={cardNumber}
+              onChange={handleCardNumber}
+              placeholder="Card number"
+            />
+          </label>
+          <label>
+            <input
+              className="w-96 rounded-lg sm:w-80"
+              type="text"
+              value={name}
+              onChange={handleName}
+              placeholder="Name on card"
+            />
+          </label>
+          <div>
             <label>
               <input
-                className="w-96 rounded-lg"
+                className="w-58 rounded-lg mr-2 sm:w-48"
                 type="text"
-                value={cardNumber}
-                onChange={handleCardNumber}
-                placeholder="Card number"
+                value={expirationDate}
+                onChange={handleExpirationDate}
+                placeholder="Expiration Date (MM / YY)"
               />
             </label>
             <label>
               <input
-                className="w-96 rounded-lg"
+                className="w-36 rounded-lg ml-1 sm:w-28"
                 type="text"
-                value={name}
-                onChange={handleName}
-                placeholder="Name on card"
+                value={securityCode}
+                onChange={handleSecurityCode}
+                placeholder="Security code"
               />
             </label>
-            <div>
-              <label>
-                <input
-                  className="w-58 rounded-lg mr-2"
-                  type="text"
-                  value={expirationDate}
-                  onChange={handleExpirationDate}
-                  placeholder="Expiration Date (MM / YY)"
-                />
-              </label>
-              <label>
-                <input
-                  className="w-36 rounded-lg ml-1"
-                  type="text"
-                  value={securityCode}
-                  onChange={handleSecurityCode}
-                  placeholder="Security code"
-                />
-              </label>
-            </div>
-            <button>Submit</button>
-          </form>
-        </div>
+          </div>
+          <button>Submit</button>
+        </form>
+        <button onClick={() => setShowCheckout(false)}>Return to cart</button>
       </div>
-    </Modal>
+    </div>
   );
 };
 
