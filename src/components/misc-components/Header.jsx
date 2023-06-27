aabd5ae47209d049b6405713eb4a5dfb235a1229
alux444 <aluux999@gmail.com>
2023-06-27 12:41:46 +1200

    added more styling

--- src/components/misc-components/Header.jsx
+++ src/components/misc-components/Header.jsx
@@ -5,7 +5,7 @@ import { UserContext, AdminContext } from "../../App";
 import logo from "../../images/kiwimartlogo.png";
 import Cart from "../store-display/cart/Cart";
 
-const Header = ({wobble, setWobble}) => {
+const Header = ({ wobble, setWobble }) => {
   const [openAbout, setOpenAbout] = useState(false);
   const [openLogin, setOpenLogin] = useState(false);
   const [goHome, setGoHome] = useState(false);
@@ -25,7 +25,10 @@ const Header = ({wobble, setWobble}) => {
       <div className="flex sm:flex-col align-center items-center justify-between lg:justify-center lg:gap-2 text-center m-2 p-5 w-[80vw] border-2 flex-wrap">
         <div className="flex align-center justify-center items-center">
           <h1 className="title">KiwiMart</h1>
-          <button className="w-[10vw] border-none p-1 bg-white" onClick={() => setAdmin(false)}>
+          <button
+            className="w-[10vw] border-none p-1 bg-white"
+            onClick={() => setAdmin(false)}
+          >
             <img src={logo} className="w-full m-0 logoicon"></img>
           </button>
         </div>
@@ -50,6 +53,15 @@ const Header = ({wobble, setWobble}) => {
         </div>
         <Cart wobble={wobble} setWobble={setWobble} />
       </div>
+      {user.loggedIn ? (
+        <div>
+          <p>Welcome to KiwiMart, {user.email.split("@")[0]}!</p>
+        </div>
+      ) : (
+        <div>
+          <p>Welcome to KiwiMart!</p>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/store-display/cart/CartItemDisplay.jsx
+++ src/components/store-display/cart/CartItemDisplay.jsx
@@ -24,22 +24,24 @@ const CartItemDisplay = ({ item }) => {
             src={item.imageUrl == "" ? image : item.imageUrl}
             className="sm:max-h-[10vh] max-h-[8vh] lg"
           />
-          <div className="flex flex-col justify-start">
-            <p>
-              {item.name} x {item.amount}
-            </p>
+          <div className="flex flex-col justify-start flex-wrap align-center sm:justify-center">
+            <p className="text-sm">{item.name}</p>
             <small className="self-start">
               ${item.price}/each = ${item.price * item.amount}
             </small>
           </div>
         </div>
         <div className="flex gap-1 align-center justify-center items-center">
-          <button className="altbutton h-min" onClick={removeAll}>
+          <button
+            className="deletebutton hover:bg-[#ec2917] hover:text-white hover:border-white h-min"
+            onClick={removeAll}
+          >
             x
           </button>
           <button className="altbutton h-min" onClick={addOneExtra}>
             +
           </button>
+          <p className="price">{item.amount}</p>
           <button className="altbutton h-min" onClick={removeOne}>
             -
           </button>
